0dba3a7612b1371f0ed132aff4687094c5aca062
Lee Ann <santore.leeann@gmail.com>
2025-11-22 23:00:52 -0500

    I added the user interface the way I understood you to want it. Let me know if this is kind of what you had in mind. If so then we can code the rest. I put up a template for the server but have only just barely started it. For the plots I just did plot1 myself but once we confirm we're on the same page we can combine yours and my code so the plots are what we want them to be.

--- Data_explore.R
+++ Data_explore.R
@@ -1,8 +1,10 @@
-getwd()
-
+###############################################################################
+#################################JR Data Import and Wrangling##################
+################################################################################
 library(tidyverse)
 library(reshape2)
 library(gmodels)
+library(shiny)
 
 #Primary data: cleaned and wrangled from multiple sources. After cleaning, I downloaded
 #and re-uploaded into this new project.
@@ -30,6 +32,181 @@ CT_washout <- CT_data %>% mutate(phone_result = case_when(phone_result == "Ineli
                               enrolled == "Yes" ~ "Eligible",
                               enrolled == "No" ~ "washout",
                               TRUE ~enrolled))
+
+###############################################################################
+######################LAS UI Creation##########################################
+##############################################################################
+
+#First I will make the user interface.
+#UI
+ui <- fluidPage(
+#I will add a title panel that describes what we are looking at with this app
+titlePanel("Project CannTalk Participant Enrollment & Washout"),
+#I will add a side bar so that users can filter data based on participant groups  
+  sidebarLayout(
+    sidebarPanel(
+#I want the sidebar to be located on the left side of the screen and take up
+#about 25% of the total screen
+      width = 3,
+#I will add each variable the user could potentially filter by here.
+
+      selectInput(
+        "age_class", "Age Group",
+        choices = c("Older-Adult", "Middle-aged", "Adult","Youth"),
+        #I have given the option to select multiple groups within each category
+        multiple = TRUE
+      ),
+      
+      selectInput(
+        "sex", "Sex",
+        choices = c("Male", "Female"),
+        multiple = TRUE
+      ),
+      
+      selectInput(
+        "gender", "Gender",
+        choices = c("Cis-Woman",
+                    "Cis-Man",
+                    "Prefer not to answer",
+                    "Multiple Identities",
+                    "Non-binary",
+                    "Genderqueer/gender non-conforming",
+                    "Transgender Woman/Trans Woman",
+                    "Transgender Man/Trans Man"),
+        multiple = TRUE
+      ),
+      
+      selectInput(
+        "race", "Race",
+        choices = c("All", "White",
+                    "Asian",
+                    "Black or African American",
+                    "Multiple Races",
+                    "Unsure",
+                    "American Indian or Alaska Native"),
+        multiple = TRUE
+      ),
+      
+      
+      selectInput(
+        "ethnicity", "Hispanic/Latinx:",
+        choices = c("No", "Yes", "Unsure"),
+        multiple = TRUE
+      ),
+      
+    ),
+    
+#Now for our plots. We want the user to see 6 different plots that
+#are arranged in two rows of 3 to the right of the category selection input pane
+#I want the plots to take up about 75% of the screen
+    
+    mainPanel(
+      width = 9,
+      
+      fluidRow(
+        column(4, plotOutput("plot1")),
+        column(4, plotOutput("plot2")),
+        column(4, plotOutput("plot3"))
+      ),
+      
+      
+      
+      fluidRow(
+        column(4, plotOutput("plot4")),
+        column(4, plotOutput("plot5")),
+        column(4, plotOutput("plot6"))
+      )
+    )
+  )
+)
+
+###############################################################################
+######################LAS Server###############################################
+###############################################################################
+
+#Now I will make the server
+server <- function(input, output, session) {
+  
+#The server has two real funcions. The first is to apply filters and the second
+#is to generate plots based on these filters.
+#Here is we will filter data based on user inputs
+  filteredData <- reactive({
+    data <- CT_washout
+#We will use "if" so that at baseline if nothing is selected by 
+#the user plots will show data from all categories
+
+if (!is.null(input$age_class) && length(input$age_class) < 
+    length(unique(CT_washout$AgeGroup))) {
+    data <- subset(data, AgeGroup %in% input$age_class)
+}
+    
+if (!is.null(input$sex) && length(input$sex) < 
+        length(unique(CT_washout$Sex))) {
+      data <- subset(data, Sex %in% input$Sex)
+    }
+    
+    
+    # TEMPLATE filter
+    if (!is.null(input$gender) && length(input$gender) < length(unique(CT_washout$Gender))) {
+      data <- subset(data, Gender %in% input$gender)
+    }
+    
+
+    
+    data
+  })
+
+#Now we will generate plots based on the applied filters  
+  
+# Plot 1
+  output$plot1 <- renderPlot({
+    filteredData() %>%
+      filter(!is.na(enrolled)) %>%
+      count(enrolled, name = "n") %>% 
+      ggplot(aes(x = "", y = n, fill = enrolled)) +
+      geom_bar(width = 1, stat = "identity") +
+      coord_polar("y", start = 0) +
+      labs(title = "Total Enrolled") +
+      theme_void() +
+      scale_fill_manual(values = c("Yes" = "#1f77b4", "No" = "#ff7f0e"))
+  })
+  
+  
+#Plot Template for others
+  output$plot2 <- renderPlot({
+    PLOT2_CODE(filteredData())
+  })
+  
+  output$plot3 <- renderPlot({
+    YOUR_PLOT3_CODE(filteredData())
+  })
+  
+  output$plot4 <- renderPlot({
+    YOUR_PLOT4_CODE(filteredData())
+  })
+  
+  output$plot5 <- renderPlot({
+    YOUR_PLOT5_CODE(filteredData())
+  })
+  
+  output$plot6 <- renderPlot({
+    YOUR_PLOT6_CODE(filteredData())
+  })
+  
+}
+
+    
+# ----- Run App -----
+shinyApp(ui = ui, server = server)
+    
+    
+    
+    
+    
+    
+###############################################################################
+#################JR original plot code#########################################
+###############################################################################
 
 #Next is plots of washout stages for each age group: plot 0 is total enrolled, yes or no. Plot
 #1 shows the number who washed out and who moved forward at the online screening stage.
