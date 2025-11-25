7e0f51ed255ff90f8dc5636722b8589df123769d
Jonathan Rascon <jonathan_rascon@brown.edu>
2025-11-24 21:47:35 -0500

    Fixed data readin

--- Data_explore.R
+++ Data_explore.R
@@ -7,8 +7,8 @@ library(tidyverse)
 library(gmodels)
 
 CT_washout <- read_csv("CT_washout.csv")
-CT_data$age_class <- factor(CT_data$age_class, levels = c("Youth", "Adult", "Middle-aged", "Older-Adult"), ordered = TRUE)
-CT_data <- CT_data %>% mutate(across(c("sex", "gender", "race", "ethnicity"), as.factor))
+CT_washout$age_class <- factor(CT_washout$age_class, levels = c("Youth", "Adult", "Middle-aged", "Older-Adult"), ordered = TRUE)
+CT_washout <- CT_washout %>% mutate(across(c("sex", "gender", "race", "ethnicity"), as.factor))
 
 ###############################################################################
 #################################JR Updated App##################
