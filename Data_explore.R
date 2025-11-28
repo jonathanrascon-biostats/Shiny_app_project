0342e417867efb8589ba12623d16936f922e8222
Jonathan Rascon <jonathan_rascon@brown.edu>
2025-11-28 10:59:59 -0500

    Added some notes, and decided to only link actionButton to test results and not plots

--- Data_explore.R
+++ Data_explore.R
@@ -25,9 +25,10 @@ ui <- page_sidebar(
                                                     "Recruitment Source" = "source"), multiple = FALSE, selected = "enrolled"),
     selectInput("factor", "Category", choices = c("Age Group" = "age_class", "Sex" = "sex","Race" = "race","Gender" = "gender", 
                                                       "Hispanic/Latinx" = "ethnicity"), multiple = FALSE, selected = "age_class"),
-    
+#actionButton is linked to the contingency table; click to generate test results   
+
     actionButton("action", "Generate"),
-#Switches for TRUE/FALSE for each test:
+#Switches for TRUE/FALSE for each test on the contingency table:
     
     input_switch(id = "expected", label = "Expected Values", value = FALSE),
     input_switch(id = "prop.r", label = "Row Proportions", value = FALSE),
@@ -40,7 +41,7 @@ ui <- page_sidebar(
     input_switch(id = "sresid", label = "Standardized Resids", value = FALSE),
     input_switch(id = "asresid", label = "Adjusted Standarized", value = FALSE)
   ),
-  #navset_card_table allows us to switch between pages, and card adds the full screen option
+  #navset_card_table allows us to switch between pages
   navset_card_tab(
     nav_panel("Description", p("Project CannTalk is a longitudinal observational study tracking habitual cannabis users over the course of approximately 9 months. 
                                With the majority of US states having adopted legislation to medically and/or recreationally legalize cannabis, public perception of 
@@ -54,29 +55,30 @@ ui <- page_sidebar(
                                their effects on CU motives, and subsequent CU. This app is meant to track recruitment efforts of the study.
                                There are many burdens-to-entry in the study: Online screening, Phone screening, Zoom Visit, In-person Visit. Of the 450 people initially eligible, only about 30% made it to enrollment.
                                This app tracks the point at which a participant washes out, i.e. the point at which they no longer progressed in the study, and those who are fully enrolled."),
-                            p("App Usage: Choose an enrollment stage (up to Total Enrollment), a Category (e.g. Age Group), and any tests to perform.The term 'washout' refers to: Lost to follow up, Ineligible, Not Interested(withdrew).
-                                  The majority of these were Lost.")),
+                            p("App Usage: Choose an enrollment stage (up to Total Enrollment), a Category (e.g. Age Group), and any tests to perform. Click 'Generate' to perform
+                               the test(s). The term 'washout' refers to: Lost to follow up, Ineligible, Not Interested(withdrew). The majority of these were Lost.")),
     nav_panel("Plots", plotOutput("plot1")),
     nav_panel("Contingency Table", verbatimTextOutput("contingency1"))
   )
 )
 server <- function(input, output, session) {
-#this reactive data frame removes NA values, and feeds into (1) summary data in the next step, and (2) into the contingency table.
-  filter_data <- eventReactive(input$action, {CT_washout %>% drop_na(all_of(input$status))}) 
+#this reactive data frame removes NA values, and feeds into (1) summary data (CT_data) in the next step, and (2) into the contingency table (CT_table).
+  filter_data <- reactive({CT_washout %>% drop_na(all_of(input$status))}) 
  #this reactive summarises the data into counts of the selected categories, and is called into the graph 
-  data <- eventReactive(input$action, {
+  CT_data <- reactive({
     CT_summary <- filter_data() %>% 
       group_by(.data[[input$status]], .data[[input$factor]]) %>% summarise(count = n(), .groups = "drop")
   })
+#this object is linked to the contingency table (contingency1), and is linked to the action button using eventReactive.
   CT_table <- eventReactive(input$action, {CrossTable(x = filter_data()[[input$status]], y = filter_data()[[input$factor]], expected = input$expected, prop.r = input$prop.r, 
                                                    prop.c = input$prop.c, prop.t = input$prop.t, prop.chisq = input$prop.chisq, chisq = input$chisq, fisher = input$fisher, 
                                                    resid = input$resid, sresid = input$sresid, asresid = input$asresid, format = "SPSS", dnn = c("Status", "Factor"))})
 #simple faceted plot that displays the various categories. Some aspects such as the legend or labels on the x-axis need to be cleaned up
   output$plot1 <- renderPlot({
-    ggplot(data(), aes(x = .data[[input$status]], y = count, fill = .data[[input$factor]]))+
+    ggplot(CT_data(), aes(x = .data[[input$status]], y = count, fill = .data[[input$factor]]))+
       geom_col(width = .5) + scale_y_continuous(n.breaks = 10) + facet_wrap(~.data[[input$factor]], scales = "free_y")
   }) 
-#output text that runs homogeneity or independence tests. Still need to add "switches" for true/false options in order to run different tests.
+#output text that runs homogeneity or independence tests. Would like to add Text to the blank start screen, e.g. 'Need to select tests"
   output$contingency1 <- renderPrint({CT_table()})
 }
 
